641c63b0a6ffa5ad3248c91d96acc4a28aef9d03
edufq <eduardofquintana@gmail.com>
2024-04-16 10:40:06 -0400

    subo todo el curso, falta la tarea que dio nicolas por realizar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // Ignorar datos que no esten en los DTO
+      forbidNonWhitelisted: true, // Lanzar error si existen datos prohibidos
+      disableErrorMessages: true, // Desabilitar mensajes de error (producción)
+    }),
+  );
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
